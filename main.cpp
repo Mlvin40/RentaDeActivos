487414c7ffeca26fc41c8950ff6a9a2e7446ff08
Melvin <melvinordonez40@gmail.com>
2024-12-11 00:56:27 -0600

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main()
+{
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
+    // <b>lang</b> variable name to see how CLion can help you rename it.
+    cout<<"Inicio del proyecto1"<<endl;
+
+    return 0;
+}
